6e2e23bf7a702684169af57e80140e4758fccb2c
Mohamed Hashem <mohamedhashem@xtrava.co>
2021-01-07 09:46:31 +0200

    Initial Commit

---
+++ MentionAndHashTagTextView/MentionAndHashTagTextView.h
@@ -0,0 +1,18 @@
+//
+//  MentionAndHashTagTextView.h
+//  MentionAndHashTagTextView
+//
+//  Created by mohamed hashem on 07/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MentionAndHashTagTextView.
+FOUNDATION_EXPORT double MentionAndHashTagTextViewVersionNumber;
+
+//! Project version string for MentionAndHashTagTextView.
+FOUNDATION_EXPORT const unsigned char MentionAndHashTagTextViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MentionAndHashTagTextView/PublicHeader.h>
+
+
